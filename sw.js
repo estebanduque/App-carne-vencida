e2b0b244162904d3f94d1c7e6701a6d28b1fc028
estebanduque <55374751+estebanduque@users.noreply.github.com>
2026-09-02 10:27:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mis-carnes-v5';
+const CACHE = 'mis-carnes-v6';
 const ASSETS = ['/App-carne-vencida/', '/App-carne-vencida/index.html', '/App-carne-vencida/manifest.json'];
  
 self.addEventListener('install', e => {
